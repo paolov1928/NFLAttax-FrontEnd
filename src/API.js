f96c791b1f0410786ca13c46c12250dd28c6ad64
paolov1928 <ventura.paolo@gmail.com>
2019-02-01 11:31:45 +0000

    added a gif to login screen. Will do something similar to the win and loss screens

--- src/API.js
+++ src/API.js
@@ -2,50 +2,52 @@
 
 class API {
   static init() {
-    this.baseURL = 'http://localhost:3000'
-    this.signinURL = this.baseURL + '/signin'
+    this.baseURL = "http://localhost:3000";
+    this.signinURL = this.baseURL + "/signin";
   }
 
   static signin(user) {
-    return fetch('http://localhost:3000/signin', {
-      method: 'POST',
+    return fetch("http://localhost:3000/signin", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         username: user.username,
         password: user.password
       })
-    }).then(resp => resp.json())
+    }).then(resp => resp.json());
   }
-
 
   static createUser(user) {
-    return fetch('http://localhost:3000/users', {
-      method: 'POST',
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
       body: JSON.stringify(user)
-    }).then(resp => resp.json())
+    }).then(resp => resp.json());
   }
-
 
   static validate() {
-    const token = localStorage.getItem('token')
-    return fetch('http://localhost:3000/validate', {
+    const token = localStorage.getItem("token");
+    return fetch("http://localhost:3000/validate", {
       headers: {
-        'Authorization': token
-      },
-      }).then(resp => resp.json())
+        Authorization: token
+      }
+    }).then(resp => resp.json());
   }
 
-
-
-
-
+  static randomGIF(number) {
+    return fetch(
+      "https://api.giphy.com/v1/gifs/search?q=NFL&api_key=" +
+        process.env.REACT_APP_GIPHY_API_KEY +
+        "&limit=" +
+        number
+    ).then(r => r.json());
+  }
 }
 
-API.init()
+API.init();
 
-export default API
+export default API;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,8 +7,13 @@ class Login extends Component {
     super(props);
     this.state = {
       username: "",
-      password: ""
+      password: "",
+      randomGIF: null
     };
+  }
+
+  componentDidMount() {
+    this.randomFootballGIF();
   }
 
   handleChange = data => {
@@ -31,10 +36,19 @@ class Login extends Component {
     });
   };
 
+  randomFootballGIF = (number = 50) => {
+    let randomNumber = Math.floor(Math.random() * number);
+    API.randomGIF(number).then(data =>
+      this.setState({
+        randomGIF: data.data[randomNumber].images.original.url
+      })
+    );
+  };
+
   render() {
     // const { history } = this.props
     // console.log(history)
-    // Was just deconstructing a variable for history that is always avaiable in props.
+    // Was just deconstructing a variable for history that is always available in props.
 
     return (
       <Container>
@@ -59,10 +73,13 @@ class Login extends Component {
           </Form.Group>
           <Form.Button>Log in</Form.Button>
         </Form>
-        <Image src="http://myorthodontists.info/wp-content/uploads/2016/09/Football-Season.jpg" />
+
+        <Image src={this.state.randomGIF} />
       </Container>
     );
   }
 }
 
 export default Login;
+
+// This was the original image <Image src="http://myorthodontists.info/wp-content/uploads/2016/09/Football-Season.jpg" />
